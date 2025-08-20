2c1854fdec002ce01773d91db6dba2b1f73c6817
Nasser <nasser.m.95@gmail.com>
2025-08-20 16:25:48 +0400

    changin port

--- app.py
+++ app.py
@@ -425,5 +425,7 @@ def clear_data():
     session.pop('sales_columns', None)
     return jsonify({'message': 'Data cleared successfully'})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("FLASK_PORT", 5001))  # use 5001 instead of 5000
+    app.run(debug=True, port=port)
